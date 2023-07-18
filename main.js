330b056f29cb2581419349cd3f88083af8eb9bf3
Dexter <mystyle1057@gmail.com>
2023-07-18 18:12:01 +0900

    commit

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var topic = require('./lib/topic');
 var author = require('./lib/author');
 
 var app = http.createServer(function(request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
+	var _url = request.url;
+	var queryData = url.parse(_url, true).query;
+	var pathname = url.parse(_url, true).pathname;
 
 	if(pathname === '/'){
 		if (queryData.id === undefined) {
