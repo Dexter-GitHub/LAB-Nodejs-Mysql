5977bc2cdabb441f9f7eb40e5dfcefa98735a349
Dexter <mystyle1057@gmail.com>
2023-07-18 18:01:19 +0900

    230718 finished

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var topic = require('./lib/topic');
+var author = require('./lib/author');
 
 var app = http.createServer(function(request, response) {
     var _url = request.url;
@@ -22,7 +23,7 @@ var app = http.createServer(function(request, response) {
 		topic.create_process(request, response);
     } 
 	else if (pathname === '/update') {		
-		topic.update(request, reEsponse);
+		topic.update(request, response);
     } 
 	else if (pathname === '/update_process') {
 		topic.update_process(request, response);
@@ -30,6 +31,21 @@ var app = http.createServer(function(request, response) {
 	else if (pathname === '/delete_process') {
 		topic.delete_process(request, response);
     } 
+	else if (pathname === '/author') {			
+		author.home(request, response);
+	}
+	else if (pathname === '/author/create_process') {
+		author.create_process(request, response);
+	}
+	else if (pathname === '/author/update') {		
+		author.update(request, response);
+	}
+	else if (pathname === '/author/update_process') {
+		author.update_process(request, response);
+	}
+	else if (pathname === '/author/delete_process') {
+		author.delete_process(request, response);
+	}
 	else {
 		response.writeHead(404);
 		response.end('Not found');
