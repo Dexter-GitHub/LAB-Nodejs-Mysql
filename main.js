229d294d79ac05a556fe567850a29d2e9e91edd5
Dexter <mystyle1057@gmail.com>
2023-07-18 18:11:28 +0900

    commit

--- main.js
+++ main.js
@@ -8,29 +8,29 @@ var app = http.createServer(function(request, response) {
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
 
-    if(pathname === '/'){
+	if(pathname === '/'){
 		if (queryData.id === undefined) {
 			topic.home(request, response);
 		} 
 		else {        
-        	topic.page(request, response);
+			topic.page(request, response);
 		}
-    } 
+	} 
 	else if (pathname === '/create') {
 		topic.create(request, response);
-    } 
+	} 
 	else if (pathname === '/create_process') {
 		topic.create_process(request, response);
-    } 
+	} 
 	else if (pathname === '/update') {		
 		topic.update(request, response);
-    } 
+	} 
 	else if (pathname === '/update_process') {
 		topic.update_process(request, response);
-    } 
+	} 
 	else if (pathname === '/delete_process') {
 		topic.delete_process(request, response);
-    } 
+	} 
 	else if (pathname === '/author') {			
 		author.home(request, response);
 	}
@@ -44,11 +44,11 @@ var app = http.createServer(function(request, response) {
 		author.update_process(request, response);
 	}
 	else if (pathname === '/author/delete_process') {
-		author.delete_process(request, response);
+		uthor.delete_process(request, response);
 	}
 	else {
 		response.writeHead(404);
 		response.end('Not found');
-    }
+	}
 });
 app.listen(3000);
